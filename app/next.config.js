35f1377866ebafc717d311b785f5d479658123da
Bebe Jane <bjornthief@gmail.com>
2021-10-23 18:51:22 +0200

    test

--- app/next.config.js
+++ app/next.config.js
@@ -2,4 +2,4 @@ const sharedDir = '/Users/bebejane/Projects/mononext/shared';
 const withPlugins = require('next-compose-plugins');
 const withTM = require('next-transpile-modules')(["../shared"]);
 
-module.exports = withPlugins([withTM], {reactStrictMode: true});
+module.exports = withPlugins([withTM], {reactStrictMode: true });
