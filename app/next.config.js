2e7b9204e2fa99abceb807e1544469f3c09d93a0
Bebe Jane <bjornthief@gmail.com>
2021-10-23 21:13:38 +0200

    test canary

--- app/next.config.js
+++ app/next.config.js
@@ -1,4 +1,6 @@
 const withPlugins = require('next-compose-plugins');
 const withTM = require('next-transpile-modules')(["../shared"]);
 
-module.exports = withPlugins([withTM], {reactStrictMode: true });
+module.exports = withPlugins([withTM], {
+  reactStrictMode: true
+});
--- app/pages/index.js
+++ app/pages/index.js
@@ -1,11 +1,11 @@
 import {Button} from '@/shared/components';
-import {formatDate} from '@/shared/lib/index';
+import { formatDate } from '@/shared/lib/index';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Button>app</Button>
+      <Button>apply</Button>
       {formatDate(new Date())}
     </div>
   )
