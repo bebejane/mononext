d1c74648327b20b313efc28d2650219325ca93ea
Bebe Jane <bjornthief@gmail.com>
2021-10-23 18:11:48 +0200

    change shared path

--- app/next.config.js
+++ app/next.config.js
@@ -1,5 +1,5 @@
 const sharedDir = '/Users/bebejane/Projects/mononext/shared';
 const withPlugins = require('next-compose-plugins');
-const withTM = require('next-transpile-modules')(["shared"]);
+const withTM = require('next-transpile-modules')(["../shared"]);
 
 module.exports = withPlugins([withTM], {reactStrictMode: true});
